f85d16ead86b2a2688caa7b73821373c56cf5343
Denis Demin <demindenis2005@gmail.com>
2023-10-12 20:59:48 +0300

    Test Deploy

--- stwar-phver/main.py
+++ stwar-phver/main.py
@@ -6,28 +6,17 @@ from phones_db import get_matching_phones, get_matching_texts
 
 @functions_framework.http
 def match_phones(request):
+    # Request structure: <Command> <input1> <input2> <input3> ...
 
-    # parse request
-    request_json = request.get_json(silent=True)
-    query_phone_list = [int(num) for num in request_json.get('phone_list')]
-    print(f'Query phones: {query_phone_list}')
-
-    # get matches
-    matching_phones = get_matching_phones(query_phone_list)
-    print(f'Matching phones: {matching_phones}')
-
-    # get texts
-    match_texts = {}
-    if matching_phones:
-        matched_categories = {m['category'] for m in matching_phones.values()}
-        match_texts = get_matching_texts(matched_categories)
-    print(f'Matching texts: {match_texts}')
-
-    # prepare response
-    response = {
-        'texts': match_texts,
-        'phone_numbers': matching_phones
-    }
-
-    print(f'Response: {response}')
-    return json.dumps(response), 200, {'ContentType': 'application/json'}
+    # Parse request
+    request_list = request.data.decode('utf-8').split(' ')
+    
+    # Get command
+    command = request_list[0]
+    
+    # Get input
+    input_list = request_list[1:]
+    
+    print(f'Command: {command}')
+    print(f'Input: {input_list}')
+    
