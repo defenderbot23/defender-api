ed91adb74258cac206d28a15b168f50cb7530535
roman.h@verbit.ai <roman.h@verbit.ai>
2023-10-13 15:33:58 +0300

    allow sending to multiple numbers

--- defender_api/send_wa/send.py
+++ defender_api/send_wa/send.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 
 import functions_framework
 
@@ -17,24 +18,36 @@ def init_client():
 @functions_framework.http
 def send_wa(request):
 
+    # prepare response
+    response = {}
+
     # parse request
     request_json = request.get_json(silent=True)
 
     # get args
-    chat_id = get_arg(request_json, 'chat_id')  # f'{phone_number}@c.us'
     message = get_arg(request_json, 'message')
+    sleep_time = get_arg(request_json, 'sleep_time', 1)
+    recipient_list = get_arg(request_json, 'recipient_list', None)   # "{phone_number}@c.us" or "{group_id}@g.us"
 
-    # send message
-    green_api = init_client()
-    resp = green_api.sending.sendMessage(
-        chatId=chat_id,
-        message=message
-    )
-    response = {
-        'code': resp.code,
-        'data': resp.data,
-        'error': resp.error
-    }
+    # iterate recipients
+    for recipient_num, recipient_id in enumerate(recipient_list):
+
+        # send message
+        green_api = init_client()
+        resp = green_api.sending.sendMessage(
+            chatId=recipient_id,
+            message=message
+        )
+
+        response[recipient_id] = {
+            'code': resp.code,
+            'data': resp.data,
+            'error': resp.error
+        }
+
+        # sleep only if there's more to send
+        if recipient_num < len(recipient_list) - 1:
+            time.sleep(sleep_time)
 
     # return
     return json.dumps(response), 200, {'ContentType': 'application/json'}
